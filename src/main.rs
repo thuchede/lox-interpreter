038968bacb483eebc1bbb958e062bb25e48043d0
thuchede <huchede.thomas@gmail.com>
2024-01-08 21:56:37 +0800

    feat: add script baseline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: rlox [script]");
+    } else if let Some(filename) = args.get(1) {
+        run_file(filename.clone())
+    } else {
+        run_prompt()
+    }
+}
+
+fn run_file(filename: String){
+    println!("Starting file {}", filename);
+}
+
+fn run_prompt(){
+    println!("Starting prompt");
 }
